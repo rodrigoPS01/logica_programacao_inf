91c886f7fd2c0c368763c285b00ec3b0e89c6801
rodrigoPS01 <145811429+rodrigoPS01@users.noreply.github.com>
2023-10-14 14:26:55 -0300

    Update quest6.py

--- python rodrigo aula3/quest6.py
+++ python rodrigo aula3/quest6.py
@@ -1,4 +1,10 @@
-for x in range(1):
-    n1 = int(input('digite um número'))
-    if n1 % 1 == 0 and n1 % n1 == 0:
-        print(f'seu número {n1} é primo')
+numero = int(input('Qual o seu númeor: '))
+n_primo = True
+for x in range(2,numero):
+    if numero % int(x) == 0:
+        n_primo = False
+    
+if n_primo == False:
+    print('O seu número não é primo')
+else:
+    print('O seu número é primo')
